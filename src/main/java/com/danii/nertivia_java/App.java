7af819d39efa797b743f53eb7ca3e078a10d2a9b
Daniihh <danielc030417@gmail.com>
2019-12-28 16:49:51 -0500

    Add SocketIO And Basics
    Gonna have to play around to understand this.

--- src/main/java/com/danii/nertivia_java/App.java
+++ src/main/java/com/danii/nertivia_java/App.java
@@ -1,10 +1,24 @@
 package com.danii.nertivia_java;
 
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
 /**
  * Hello world!
  */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args) throws URISyntaxException {
+		IO.Options options = new IO.Options();
+		options.query = "sid=";
+
+		Socket socket = IO.socket("https://nertivia.supertiger.tk/", options);
+		socket.on("new message", (data) -> System.out.println(data));
+		socket.on("connect_error", (data) -> System.out.println(Arrays.toString(data)));
+		System.out.println(socket);
+
+		socket.open();
 	}
 }
